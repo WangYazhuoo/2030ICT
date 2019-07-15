6f0127f61b059f9ee999da83f734c77b50b5eaf0
Wang Yazhuo <yazhuo.wang@griffithuni.edu.au>
2019-07-15 14:47:25 +1000

    change

---
+++ Untitled1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[3]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+print("Hello World")
+
+
+# In[7]:
+
+
+x = 1 + 1
+print(x)
+
+
+# In[8]:
+
+
+x = 2
+y = 3
+
+
+# In[10]:
+
+
+z = x + y
+print(z)
+
+
+# In[12]:
+
+
+1 < 3
+
+
+# In[13]:
+
+
+1 > 3
+
+
+# In[14]:
+
+
+1 == 3
+
+
+# In[ ]:
+
+
+
+
